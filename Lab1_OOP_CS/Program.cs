baeb8f9d9ff1167bde402daf0745c3ed58bbc12f
maximu211 <63352037+maximu211@users.noreply.github.com>
2023-03-01 11:45:42 +0200

    Update Program.cs

--- Lab1_OOP_CS/Program.cs
+++ Lab1_OOP_CS/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Lab1_OOP_CS
 {
@@ -6,33 +6,40 @@ namespace Lab1_OOP_CS
     {
         static void Main(string[] args)
         {
-            Time time = new Time(); //створення об'єкту 
-            while (true)
+            Time time = new Time(); //створення об'єкту
+            try
             {
-                Console.WriteLine("виберіть операцію: " + //меню у безкінечному циклі
-                    " 0 - встановлення години"+
-                    " 1 - встановлення хвилини"+
-                    " 2 - встановлення секунди" +
-                    " 3 - вихід з програми");
-                int opperation = Convert.ToInt16(Console.ReadLine());
-                switch(opperation)
+                while (true)
                 {
-                    case 0: //встановлення години
-                        time.SetHour();
-                        time.DisplayTime();
-                        break;
-                    case 1: //встановлення хвилин
-                        time.SetMin();
-                        time.DisplayTime();
-                        break;
-                    case 2: //встановлення секунд
-                        time.SetSec();
-                        time.DisplayTime();
-                        break;
-                    case 3: //вихід
-                        Environment.Exit(0);
-                        break;
+                    Console.WriteLine("виберіть операцію: " + //меню у безкінечному циклі
+                        " 0 - встановлення години" +
+                        " 1 - встановлення хвилини" +
+                        " 2 - встановлення секунди" +
+                        " 3 - вихід з програми");
+                    int opperation = Convert.ToInt16(Console.ReadLine());
+                    switch (opperation)
+                    {
+                        case 0: //встановлення години
+                            time.SetHour();
+                            time.DisplayTime();
+                            break;
+                        case 1: //встановлення хвилин
+                            time.SetMin();
+                            time.DisplayTime();
+                            break;
+                        case 2: //встановлення секунд
+                            time.SetSec();
+                            time.DisplayTime();
+                            break;
+                        case 3: //вихід
+                            Environment.Exit(0);
+                            break;
+                    }
                 }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Помилка");
             }
         }
     }
