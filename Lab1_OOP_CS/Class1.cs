1cda862d454c6b3ec0e8c98602778c960e20cb70
maximu211 <63352037+maximu211@users.noreply.github.com>
2023-03-01 10:11:42 +0200

    Add files via upload

---
+++ Lab1_OOP_CS/Class1.cs
@@ -0,0 +1,109 @@
+﻿public class Time
+{
+    private int hours; //поля
+    private int minutes;
+    private int seconds;
+
+    public Time(int hours, int minutes, int seconds) //конструктор
+    {
+        Hours = hours;
+        Minutes = minutes;
+        Seconds = seconds;
+    }
+
+    public int Hours //get set годин
+    {
+        get { return hours; }
+        set
+        {
+            if (value >= 0 && value <= 23)
+            {
+                hours = value;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid value for hours.");
+            }
+        }
+    }
+
+    public int Minutes //get set хвилин
+    {
+        get { return minutes; }
+        set
+        {
+            if (value >= 0 && value <= 59)
+            {
+                minutes = value;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid value for minutes.");
+            }
+        }
+    }
+
+    public int Seconds //get set секунд
+    {
+        get { return seconds; }
+        set
+        {
+            if (value >= 0 && value <= 59)
+            {
+                seconds = value;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid value for seconds.");
+            }
+        }
+    }
+
+    public void SetHour() //функція зміни годин
+    {
+        Console.WriteLine("Введіть годину");
+        int value = Convert.ToInt32(Console.ReadLine());
+        if (value >= 0 && value <= 24)
+        {
+            hours = value; 
+        }
+        else
+        {
+            Console.WriteLine("Помилка, введено не коректні дані");
+        }
+    }
+
+    public void SetMin() //функція зміни хвилин
+    { 
+        Console.WriteLine("Введіть хвилину");
+        int value = Convert.ToInt32(Console.ReadLine());
+        if (value >= 0 && value <= 59)
+        {
+           minutes = value;
+        }
+        else
+        {
+            Console.WriteLine("Помилка, введено не коректні дані");
+        }
+    }
+
+    public void SetSec() //функція зміни секунд
+    {
+        Console.WriteLine("Введіть секунди");
+        int value = Convert.ToInt32(Console.ReadLine());
+        if (value >= 0 && value <= 59)
+        {
+            seconds = value;
+        }
+        else
+        {
+            Console.WriteLine("Помилка, введено не коректні дані");
+        }
+    }
+
+    public void DisplayTime() // вивід поточної години
+    {
+
+        Console.WriteLine("Поточна година: " + hours + ":" + minutes + ":" + seconds);
+    }
+}
---
+++ Lab1_OOP_CS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab1_OOP_CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time = new Time(0, 0, 0); //створення об'єкту із початковими параметрами
+            while (true)
+            {
+                Console.WriteLine("виберіть операцію: " + //меню у безкінечному циклі
+                    " 0 - встановлення години"+
+                    " 1 - встановлення хвилини"+
+                    " 2 - встановлення секунди" +
+                    " 3 - вихід з програми");
+                int opperation = Convert.ToInt16(Console.ReadLine());
+                switch(opperation)
+                {
+                    case 0: //встановлення години
+                        time.SetHour();
+                        time.DisplayTime();
+                        break;
+                    case 1: //встановлення хвилин
+                        time.SetMin();
+                        time.DisplayTime();
+                        break;
+                    case 2: //встановлення секунд
+                        time.SetSec();
+                        time.DisplayTime();
+                        break;
+                    case 3: //вихід
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}
